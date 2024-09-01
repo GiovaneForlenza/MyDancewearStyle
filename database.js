3acf6a098516329201ed4ec4c98cc7eccb49a236
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2024-09-01 12:51:41 -0300

    Basic hard coded structure done

---
+++ database.js
@@ -0,0 +1,24 @@
+export const STUDIOS = [
+  { id: 1, name: "Dance with mara" },
+  { id: 2, name: "ADAZ" },
+  { id: 3, name: "Evolve" },
+  { id: 4, name: "Kids in Dance" },
+  { id: 5, name: "Dance with mara" },
+  { id: 6, name: "Tracy Stones" },
+];
+
+export const RECITALS = [
+  { id: 1, studioId: 1, name: "Recital", year: 2024 },
+  { id: 2, studioId: 1, name: "Recital", year: 2023 },
+  { id: 3, studioId: 2, name: "Star Power", year: 2024 },
+  { id: 4, studioId: 2, name: "Showstopper", year: 2024 },
+  { id: 5, studioId: 2, name: "Adrenaline", year: 2024 },
+  { id: 6, studioId: 2, name: "National Dance Honors", year: 2024 },
+  { id: 7, studioId: 2, name: "40th Recital", year: 2024 },
+  { id: 8, studioId: 3, name: "On The Floor", year: 2024 },
+  { id: 9, studioId: 3, name: "On The Floor / Dance Battle", year: 2024 },
+  { id: 10, studioId: 4, name: "Move Dance Challenge", year: 2024 },
+  { id: 11, studioId: 4, name: "View Dance Challenge", year: 2024 },
+  { id: 12, studioId: 5, name: "Headliners", year: 2024 },
+  { id: 13, studioId: 5, name: "VIP Dance", year: 2024 },
+];
