8c510490c7d9ab809a9bfb176f43e08713722ffa
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2024-09-02 18:30:18 -0300

    Working on the recitals page

--- navbar.js
+++ navbar.js
@@ -1,4 +1,4 @@
-document.write(`<div class="navbar">
- 
- 
- </div>`);
+document.write(`
+ <div class="navbar" onclick="window.location.href = '../index.html'">
+      <i class="fa-solid fa-house"></i>
+    </div>`);
