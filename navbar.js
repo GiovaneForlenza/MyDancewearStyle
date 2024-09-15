a340ff94d444dff304b89e54b962228e9e1f3b5f
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2024-09-15 13:46:55 -0300

    More work on responsive, and added tooltips for the nav icons

--- navbar.js
+++ navbar.js
@@ -1,4 +1,14 @@
-document.write(`
- <div class="navbar" onclick="window.location.href = '../index.html'">
-      <i class="fa-solid fa-house"></i>
-    </div>`);
+document.write(`<div class="navbar" onclick="window.location.href = '../index.html'">
+  <span data-tooltip="Select Studio" data-flow="right">
+    <i class="fa-solid fa-lg fa-archive"></i>
+  </span>
+  <span
+    data-tooltip="Return to last watched studio (KID default)"
+    data-flow="right"
+  >
+    <i class="fa-solid fa-house"></i>
+  </span>
+  <span data-tooltip="Logout" data-flow="right">
+    <i class="fa-solid fa-lg fa-arrow-right-from-bracket"></i>
+  </span>
+</div>`);
